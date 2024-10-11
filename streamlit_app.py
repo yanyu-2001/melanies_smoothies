3e77dff7766658a35a41f2d3b3ae63fecbc9fd65
yanyu-2001 <yanyu@rockborne.com>
2024-10-11 10:39:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,15 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         fruityvice_response = requests.get(#"https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
+        my_insert_stmt = (
+                "insert into smoothies.public.orders(ingredients, name_on_order) "
+                "values ('" + ingredients_string + "', '" + name_on_order + "')"
+            )
+
             
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+        #my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+           # values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
     time_to_insert = st.button('Submit Order')
 
