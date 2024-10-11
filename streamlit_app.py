55168e59138cbaad04bd2960457cec8d3925832e
yanyu-2001 <yanyu@rockborne.com>
2024-10-11 09:42:59 +0100

    Update streamlit_app.py
    added pip install to try correct streamlit error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
+pip install snowflake-connector-python
+pip install "snowflake-snowpark-python[pandas]"
+
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
