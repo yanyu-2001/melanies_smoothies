dff8607fd83f6b4a85cfa03a5662ed6324ffec4b
yanyu-2001 <yanyu@rockborne.com>
2024-10-11 10:12:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -42,7 +43,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 # New section to display fruityvice nutrition information
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+st.text(fruityvice_response)
